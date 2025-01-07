b7f89a3274ea0c889adaa44a94cb55c93b5acc55
serdar1234 <serdarabad@gmail.com>
2025-01-07 20:18:33 +0300

    repos are added without errors

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 class Search {
   constructor() {
     this.app = document.querySelector(".app");
-    this.input = this.makeElement("input", "searchField");
-    this.autocomplete = this.makeElement("ul", "autocomplete");
-    this.repoWrapper = this.makeElement("div", "repoWrapper");
+    this.input = makeElement("input", "searchField");
+    this.autocomplete = makeElement("ul", "autocomplete");
+    this.repoWrapper = makeElement("div", "repoWrapper");
+    this.githubSearchObject = null;
 
     this.svgCross = `<svg height="48" width="48" xmlns="http://www.w3.org/2000/svg">
       <line x1="3" y1="3" x2="42" y2="42" style="stroke:red;stroke-width:4"/>
@@ -18,9 +19,7 @@ class Search {
     this.app.append(this.repoWrapper);
 
     this.input.addEventListener("input", this.debouncedSearch());
-
-    const demoText = `Name: react<br>Owner: facebook<br>
-    Stars: 145231`;
+    this.autocomplete.addEventListener("click", onLiClick.bind(this));
   }
 
   debouncedSearch() {
@@ -46,50 +45,53 @@ class Search {
         throw new Error(`HTTP error: ${githubSearchJSONstring.status}`);
       }
 
-      const githubSearchObject = await githubSearchJSONstring.json();
+      this.githubSearchObject = await githubSearchJSONstring.json();
       if (this.autocomplete) this.autocomplete.textContent = "";
-      githubSearchObject.items.forEach((el) => {
-        this.autocomplete__item = this.makeElement("li");
+      this.githubSearchObject.items.forEach((el) => {
+        this.autocomplete__item = makeElement("li");
         this.autocomplete__item.innerHTML = el.name;
         this.autocomplete.append(this.autocomplete__item);
-      });
-      this.autocomplete.addEventListener("click", async (evt) => {
-        const targetLi = evt.target.closest("li");
-        console.log(targetLi.innerHTML);
-        this.repa = this.makeElement("div", "repa");
-        this.stats = this.makeElement("div", "stats");
-        this.repoWrapper.append(this.repa);
-        this.repa.append(this.stats);
-        this.cross = this.makeElement("img", "cross");
-        this.cross.src = this.dataURL;
-        this.repa.append(this.cross);
-        const targetObj = githubSearchObject.items.find(
-          (repo) => repo.name === targetLi.textContent
-        );
-        this.stats.insertAdjacentHTML(
-          "beforeend",
-          `
-          <p>Name: ${targetObj.name}<br />
-          Owner: ${targetObj.owner.login}<br />
-          Stars: ${targetObj.stargazers_count}</p>
-          `
-        );
       });
     } catch (error) {
       console.error("Error:", error);
     }
   }
-
-  makeElement(tagName, className) {
-    const newElement = document.createElement(tagName);
-    if (className) {
-      newElement.classList.add(className);
-    }
-    return newElement;
-  }
 }
 
 new Search();
+
+function onLiClick(evt) {
+  const targetLi = evt.target.closest("li");
+  console.log(targetLi.innerHTML);
+  this.repa = makeElement("div", "repa");
+  this.stats = makeElement("div", "stats");
+  const repoWrapper = document.querySelector(".repoWrapper");
+  repoWrapper.append(this.repa);
+  this.repa.append(this.stats);
+  this.cross = makeElement("img", "cross");
+  this.cross.src = this.dataURL;
+  this.repa.append(this.cross);
+  const targetObj = this.githubSearchObject.items.find(
+    (repo) => repo.name === targetLi.textContent
+  );
+  this.stats.insertAdjacentHTML(
+    "beforeend",
+    `
+    <p>Name: ${targetObj.name}<br />
+    Owner: ${targetObj.owner.login}<br />
+    Stars: ${targetObj.stargazers_count}</p>
+    `
+  );
+}
+
+function makeElement(tagName, className) {
+  const newElement = document.createElement(tagName);
+
+  if (className) {
+    newElement.classList.add(className);
+  }
+  return newElement;
+}
 
 function debounce(fn, ms) {
   let timerID = null;
